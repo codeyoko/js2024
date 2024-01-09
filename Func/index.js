a1df75374f3c9fa054f03fd7ab8bac3adb3dfbf0
codeyoko <codeyoko.gka@gmail.com>
2024-01-09 17:01:33 +0900

    add filter

--- Func/index.js
+++ Func/index.js
@@ -9,3 +9,20 @@ let callbackFunc = (sum) => {
 }
 
 a(5,7,callbackFunc)
+
+
+// filter, find
+
+let arr = [1,2,3,4,5,6,7,8];
+let arrObj = [
+    {name: "Join", age:30},
+    {name: "Marry", age:30},
+    {name: "Ross", age:40},
+];
+
+let arrFilter1 = arrObj.find((item) =>  { return item && item.age === 30; })
+    
+let arrFilter = arrObj.filter((item) =>  { return item && item.age === 30; })
+
+console.log(arrFilter1);
+console.log(arrFilter);
